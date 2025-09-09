7282fdfdd3f960de10f0660bc57e17af7a4f9147
joric <joric@users.noreply.github.com>
2025-09-09 08:00:36 +0500

    updated to 7925

--- scripts/do_gzip.sh
+++ scripts/do_gzip.sh
@@ -3,5 +3,6 @@
 #tar c resources | gzip --best > best.file.tar.gz
 #tar cvf - resources | gzip -9 - > new.file.tar.gz
 
-gzip -v9 -c Supraworld.json > Supraworld.gz
+build=7925
+gzip -v9 -c /mnt/c/Temp/Exports/Supraworld/Plugins/GameFeatures/Supraworld/Supraworld/Content/Maps/Supraworld.json > Supraworld.$build.json.gz
 
