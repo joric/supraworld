8e341c9b8073d6fa57cce0c04774b9df9e8ecf6c
joric <joric@users.noreply.github.com>
2025-09-11 10:33:45 +0500

    updated files

--- scripts/ue4ss/Mods/Supraworld/Scripts/noclip.test.lua
+++ scripts/ue4ss/Mods/Supraworld/Scripts/noclip.test.lua
@@ -21,6 +21,13 @@ NotifyOnNewObject("/Script/Engine.PlayerController", function(PlayerController)
     return cheatable(PlayerController)
 end)
 
+local function fillSuit()
+    local suit = FindFirstOf("Equippable_SpongeSuit_C")
+    if suit then
+        suit:SetCurrentFill(1.0)
+    end
+end
+
 local function removeBlockingVolumes()
     for _, actor in ipairs(FindAllOf("SupraEABlockingVolume_C") or {}) do
         if actor:IsValid() then
@@ -63,13 +70,6 @@ local function start(pc)
     pc:EnableInput(pc)
 end
 
-local function fillSuit()
-    local suit = FindFirstOf("Equippable_SpongeSuit_C")
-    if suit then
-    suit:SetCurrentFill(1.0)
-    end
-end
-
 local function toggleDebugCamera()
     local dc = getDebugCameraController()
     local pc = UEHelpers.GetPlayerController()
@@ -89,9 +89,37 @@ local function toggleDebugCamera()
     end
 end
 
-local function teleportToTrace(CameraController, PlayerController)
-    local GetKismetSystemLibrary = UEHelpers.GetKismetSystemLibrary
-    local GetKismetMathLibrary = UEHelpers.GetKismetMathLibrary
+local GetKismetSystemLibrary = UEHelpers.GetKismetSystemLibrary
+local GetKismetMathLibrary = UEHelpers.GetKismetMathLibrary
+local GetGameplayStatics = UEHelpers.GetGameplayStatics
+
+function IsValid(object)
+    return object ~= nil and object.IsValid ~= nil and object:IsValid()
+end
+
+function IsNotValid(object)
+    return not IsValid(object)
+end
+
+function FVector(X, Y, Z)
+    X = X or 0.0
+    Y = Y or 0.0
+    Z = Z or 0.0
+    return {
+        X = X,
+        Y = Y,
+        Z = Z
+    }
+end
+
+local function getTargetPoint()
+    local PlayerController = UEHelpers.GetPlayerController()
+    local CameraController = PlayerController
+    if inDebugCamera then
+        CameraController = getDebugCameraController()
+    end
+
+    if not PlayerController or not CameraController then return {X=0,Y=0,Z=0} end
 
     local PlayerPawn = PlayerController.Pawn
     local CameraManager = CameraController.PlayerCameraManager
@@ -127,22 +155,19 @@ local function teleportToTrace(CameraController, PlayerController)
     )
     if WasHit then
         local loc = HitResult.ImpactPoint
-        local rot = CameraManager:GetCameraRotation()
-
-        -- local res = PlayerPawn:K2_TeleportTo(loc, rot) -- this sometimes fails because of sweep
-
-        local res = PlayerPawn:K2_SetActorLocation(loc, false, {}, false)
-
-        print("teleporting to", loc.X, loc.Y, loc.Z, rot, res)
-    else
-        print("hit failed")
+        print("ImpactPoint", loc.X, loc.Y, loc.Z)
+        return loc
     end
+    return {X=0,Y=0,Z=0}
 end
 
-
+local function teleportToTrace(PlayerPawn)
+    local loc = getTargetPoint()
+    local res = PlayerPawn:K2_SetActorLocation(loc, false, {}, false)
+    print("teleportToTrace", loc.X, loc.Y, loc.Z, rot, res)
+end
 
 local function teleportPlayer()
-
     fillSuit()
 
     if not inDebugCamera then return end
@@ -166,13 +191,10 @@ local function teleportPlayer()
             if (os.clock() - (lastTime or 0)) * 1000 < throttleMs then return end
             lastTime = os.clock()
 
-            local cam = dc.PlayerCameraManager
-
             -- pc.Pawn:K2_TeleportTo(cam:GetCameraLocation(), cam:GetCameraRotation()) -- camera position
             -- getDebugCameraController().CheatManager:Teleport() -- uses sweep, needs line of sight
 
-
-            teleportToTrace(dc, pc) -- may hit hidden volumes
+            teleportToTrace(pc.Pawn) -- may hit hidden volumes
 
             start(pc)
 
@@ -182,5 +204,83 @@ local function teleportPlayer()
     end)
 end
 
+
+function SpawnActorFromClass(ActorClassName, Location, Rotation)
+    local invalidActor = CreateInvalidObject() ---@cast invalidActor AActor
+    if type(ActorClassName) ~= "string" or not Location then return invalidActor end
+    Rotation = Rotation or FRotator()
+
+    LoadAsset(ActorClassName)
+
+    local kismetMathLibrary = GetKismetMathLibrary()
+    local gameplayStatics = GetGameplayStatics()
+
+    if not kismetMathLibrary or not gameplayStatics then return invalidActor end
+
+    local world = UEHelpers.GetWorld()
+    if IsNotValid(world) then return invalidActor end
+
+    local actorClass = StaticFindObject(ActorClassName)
+    if IsNotValid(actorClass) then
+        print("SpawnActorFromClass: Couldn't find static object:", ActorClassName)
+        return invalidActor
+    end
+
+    -- local transform = {Rotation=rot, Translation=loc, Scale3D={X=1, Y=1, Z=1}}
+    local transform = kismetMathLibrary:MakeTransform(Location, Rotation, FVector(1.0, 1.0, 1.0))
+
+    --[[
+    print("SpawnActorFromClass: UWorld: " .. type(world))
+    print("SpawnActorFromClass: class: " .. actorClass:type())
+    print("SpawnActorFromClass: transform: " .. type(transform))
+
+    for key, value in pairs(transform) do
+        print("key:", key, "value:", value)
+        for k, v in pairs(value) do
+            print("  k:", k, "v:", v)
+        end
+    end
+    ]]
+
+    local deferredActor  = gameplayStatics:BeginDeferredActorSpawnFromClass(world, actorClass, transform, 0, nil, 0)
+    if IsValid(deferredActor) then
+        print("SpawnActorFromClass: Deferred Actor successfully")
+        return gameplayStatics:FinishSpawningActor(deferredActor, transform, 0)
+    end
+    return invalidActor
+end
+
+
+local function spawnObject(className)
+    local loc = getTargetPoint()
+    local rot = {Pitch=0, Yaw=0, Roll=0}
+
+    ExecuteWithDelay(250, function()
+        ExecuteInGameThread(function()
+            print('spawning actor')
+            SpawnActorFromClass(className, loc, rot)
+        end)
+    end)
+
+end
+
+local function dumpObjects()
+    for _,obj in pairs(FindAllOf("Object")) do
+        local name = obj:GetFullName()
+        if name:find("Carriables") and name:find("Battery_C") then
+            print(name)
+        end
+    end
+end
+
+local function spawnThings()
+    -- spawnObject('/Supraworld/Abilities/Spark/Inventory_Spark.Inventory_Spark_C') -- cant' really spawn abilities, need spawner
+
+    spawnObject('/Supraworld/Levelobjects/Carriables/ButtonBattery.ButtonBattery_C')
+end
+
+
 RegisterKeyBind(Key.MIDDLE_MOUSE_BUTTON, toggleDebugCamera)
 RegisterKeyBind(Key.LEFT_MOUSE_BUTTON, teleportPlayer)
+RegisterKeyBind(Key.RIGHT_MOUSE_BUTTON, spawnThings)
+
