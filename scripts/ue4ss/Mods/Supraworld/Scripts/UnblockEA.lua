d26d6b86d108cd7e8208c4c034534aa5358abd5a
joric <joric@users.noreply.github.com>
2025-09-12 21:18:01 +0500

    updated files

--- scripts/ue4ss/Mods/Supraworld/Scripts/UnblockEA.lua
+++ scripts/ue4ss/Mods/Supraworld/Scripts/UnblockEA.lua
@@ -3,7 +3,6 @@
 local function unblockEA()
     for _, actor in ipairs(FindAllOf("SupraEABlockingVolume_C") or {}) do
         if actor:IsValid() then
-            -- actor:K2_DestroyActor()
             actor:SetActorEnableCollision(false)
         end
     end
