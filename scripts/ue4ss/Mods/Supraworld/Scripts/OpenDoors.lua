e5e960d8fd307c7bc2f4258d7445b63f059f7fc0
joric <joric@users.noreply.github.com>
2025-09-13 14:20:39 +0500

    updated files

--- scripts/ue4ss/Mods/Supraworld/Scripts/OpenDoors.lua
+++ scripts/ue4ss/Mods/Supraworld/Scripts/OpenDoors.lua
@@ -18,6 +18,7 @@ local function openDoor()
                 local method = parent[methodName]
                 if method and method:IsValid() then
                     method(parent)
+                    return
                 end
             end
         end
