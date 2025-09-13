d7fc2edd4a17a3a39418d499217b8766d0c90c36
joric <joric@users.noreply.github.com>
2025-09-13 10:43:49 +0500

    updated files

--- scripts/ue4ss/Mods/Supraworld/Scripts/FastTravel.lua
+++ scripts/ue4ss/Mods/Supraworld/Scripts/FastTravel.lua
@@ -2,9 +2,9 @@
 
 local UEHelpers = require("UEHelpers")
 
-local function getFloorHeight(PlayerPawn, worldX, worldY)
-    local StartVector = {X = worldX, Y = worldY, Z = 10000}
-    local Rotation = { Pitch = -90, Yaw = 0, Roll = 0 } -- points down
+local function getFloorHeight(PlayerPawn, worldX, worldY, maxHeight)
+    local StartVector = {X = worldX, Y = worldY, Z = maxHeight}
+    local Rotation = { Pitch = -90, Yaw = 0, Roll = 0 }
     return getImpactPoint(PlayerPawn, StartVector, Rotation).Z
 end
 
@@ -22,27 +22,22 @@ local function fastTravel()
 
             local widget = FindFirstOf("SW_PlayerMapWidget_C")
             if not widget or not widget:IsValid() then
-                widget = FindFirstOf("PlayerMapWidget_C") -- sl/siu
+                widget = FindFirstOf("PlayerMapWidget_C")
+                if not widget or not widget:IsValid() then return end
             end
-
-            if not widget or not widget:IsValid() then return end
 
             local mapActor = FindFirstOf("PlayerMapActor_C")
             if not mapActor or not mapActor:IsValid() then return end
 
-            local size = mapActor.MapWorldSize
-            local cx = mapActor.MapWorldCenter.X
-            local cy = mapActor.MapWorldCenter.Y
-
             local virtualMap = {}
             local mapLocation = {}
             local ok = widget:GetMousePositionOnVirtualMap(virtualMap, mapLocation)
-
             if not ok or (virtualMap.X==0 and virtualMap.Y==0) then return end
 
-            local worldX = (mapLocation.X - 0.5) * size + cx
-            local worldY = (mapLocation.Y - 0.5) * size + cy
-            local floorHeight = getFloorHeight(pc.Pawn, worldX, worldY)
+            local worldX = (mapLocation.X - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.X
+            local worldY = (mapLocation.Y - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.Y
+
+            local floorHeight = getFloorHeight(pc.Pawn, worldX, worldY, mapActor.CaptureHeight)
             local loc = {X = worldX, Y = worldY, Z = floorHeight + 100}
 
             pc.Pawn:K2_TeleportTo(loc, pc.Pawn:K2_GetActorRotation())
