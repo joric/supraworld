c828006d128f1a8ea7d0ba0d029d27f16580a4bf
joric <joric@users.noreply.github.com>
2025-09-13 12:17:06 +0500

    updated files

--- scripts/ue4ss/Mods/Supraworld/Scripts/FastTravel.lua
+++ scripts/ue4ss/Mods/Supraworld/Scripts/FastTravel.lua
@@ -36,6 +36,7 @@ local function fastTravel()
 
             local worldX = (mapLocation.X - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.X
             local worldY = (mapLocation.Y - 0.5) * mapActor.MapWorldSize + mapActor.MapWorldCenter.Y
+
             local maxHeight = mapActor.CaptureHeight - mapActor.MapWorldCenter.Z + mapActor.MapWorldUpperLeft.Z
 
             local floorHeight = getFloorHeight(pc.Pawn, worldX, worldY, maxHeight)
