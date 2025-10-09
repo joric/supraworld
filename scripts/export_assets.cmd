c9423ad31499435086bff6da149e395f76c9144f
joric <joric@users.noreply.github.com>
2025-10-09 16:31:55 +0500

    updated files

--- scripts/export_assets.cmd
+++ scripts/export_assets.cmd
@@ -2,9 +2,8 @@
 
 rem download CUE4Parse.CLI here: https://github.com/joric/CUE4Parse.CLI
 
-set out=C:\Temp\Exports
-
 set root=E:\Games\Supraworld
+set out=C:\Temp\Exports
 
 set opt=-i "%root%" -m "%root%\Mappings.usmap" -g GAME_UE5_6 -o "%out%" -v
 
