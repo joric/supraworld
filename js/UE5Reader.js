2b8e4adca517b1b25d9a67d72adcf20b55a0d344
joric <joric@users.noreply.github.com>
2026-04-23 13:37:22 +0500

    updated files

--- js/UE5Reader.js
+++ js/UE5Reader.js
@@ -2,46 +2,38 @@
 // joric, 2026
 
 class UESaveObject {
-  constructor(arrayData) {
-    let dataview = new DataView(arrayData, arrayData.byteOffset, arrayData.byteLength);
-    let strview = new TextDecoder("latin1").decode(dataview);
-
-    const srchStr = '\0PersistentLevel.'
-    const re_match = new RegExp(srchStr, 'gi');
-    let m;
-
+  constructor(binstr) {
+    const dataview = new DataView(binstr, binstr.byteOffset, binstr.byteLength);
+    const strview = new TextDecoder("latin1").decode(dataview);
+    const prefix = '\0PersistentLevel.';
+    const regex = new RegExp(prefix, 'gi');
+    
     this.Properties = [];
-    this.Properties.push({"name": "ThingsToRemove", "value": {"value": []}});
-    let o = this.Properties[0].value.value;
-
-    while ((m = re_match.exec(strview)) != null) {
-        const nameidx = m.index + srchStr.length;
-        const namelen = dataview.getInt32(m.index+1-4, true) - srchStr.length + 1;
-        const name = strview.slice(nameidx, nameidx + namelen);
-        o.push(name);
+    this.Properties.push({ name: "ThingsToRemove", value: { value: [] } });
+    const o = this.Properties[0].value.value;
+    
+    let match;
+    while ((match = regex.exec(strview))) {
+      const start = match.index + prefix.length;
+      const len = dataview.getInt32(match.index - 3, true) - prefix.length + 1;
+      o.push(strview.slice(start, start + len));
     }
   }
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
   for (fname of [
-    "C:\\Users\\user\\AppData\\Local\\Supraworld\\Saved\\SaveGames\\Supraworld\\6\\SaveGame.sav"
+    //"C:\\Users\\user\\AppData\\Local\\Supraworld\\Saved\\SaveGames\\Supraworld\\6\\SaveGame.sav"
+    "SaveGame.sav"
   ])
   require('fs').readFile(fname, (err, buf) => {
     if (err) {
       console.log(err);
     } else {
-      let loadedSave = new UE5SaveObject(buf.buffer);
+      let loadedSave = new UESaveObject(buf.buffer);
       let s = JSON.stringify(loadedSave,null,2);
       require('fs').writeFileSync('save.json', s);
       console.log(s);
-      /*
-      for (o of loadedSave.Properties) {
-        if (o.name == 'Player Position') {
-          console.log(JSON.stringify(o, null, 2));
-        }
-      }
-      */
     }
   })
 }
